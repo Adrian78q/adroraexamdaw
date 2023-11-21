8c561a7a39a36dcfb01047b1bb6bd95ed6878181
Adrian78q <adrora@floridauniversitaria.es>
2023-11-21 12:30:18 +0100

    first commit

--- database/adrora.sql
+++ database/adrora.sql
@@ -1,7 +1,10 @@
-CREATE TABLE "persona" (
-  "nombre" varchar(20) NOT NULL,
-  "edad" int(20) NOT NULL,
-  "estilo_musica" varchar(20) NOT NULL,
-)
+CREATE DATABASE IF NOT EXISTS `adrora`;
+USE `adrora`;
 
-INSERT INTO "persona" ("nombre", "edad", "estilo_musica") VALUES ("Adrian Rodriguez Rayo", 20, "Rap"), ("Antonio Oinotna", 40, "Tecno");
+CREATE TABLE `persona` (
+  `nombre` varchar(20) NOT NULL,
+  `edad` int(20) NOT NULL,
+  `estilo_musica` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `persona` (`nombre`, `edad`, `estilo_musica`) VALUES (`Adrian Rodriguez Rayo`, 20, `Rap`), (`Antonio Oinotna`, 40, `Tecno`);
